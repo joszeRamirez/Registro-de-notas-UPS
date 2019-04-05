b25364f6764fa9845ea6fae812a093230209b0f7
Josze <jramirezc9@est.ups.edu.ec>
2019-04-05 11:27:19 -0500

    Adicion del principal con prueba de estudiante

--- src/ec/edu/ups/clases/Persona.java
+++ src/ec/edu/ups/clases/Persona.java
@@ -41,42 +41,92 @@ public class Persona {
         return codigo;
     }
 
+    /**
+     * Método que introde la información al objeto codigo
+     *
+     * @param codigo codigo de la persona
+     */
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
 
+    /**
+     * Método que devuelve el nombre de la persona
+     *
+     * @return (String) nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Método que introduce la información al objeto nombre
+     *
+     * @param nombre nombre de la persona
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Método que devuelve la cedula de la persona
+     *
+     * @return (String) cedula
+     */
     public String getCedula() {
         return cedula;
     }
 
+    /**
+     * Método que introduce la información al objeto cedula
+     *
+     * @param cedula cedula de la persona
+     */
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
+    /**
+     * Método que devuelve el telefono de la persona
+     *
+     * @return (String) telefono
+     */
     public String getTelefono() {
         return telefono;
     }
 
+    /**
+     * Método que introduce la información al objeto telefono
+     *
+     * @param telefono telefono de la persona
+     */
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
+    /**
+     * Método que devuelve la direccion de la persona
+     *
+     * @return (String) direccion
+     */
     public String getDireccion() {
         return direccion;
     }
 
+    /**
+     * Método que introduce la información al objeto direccion
+     *
+     * @param direccion direccion de la persona
+     */
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    /**
+     * Método que devuelve el correo de la persona
+     *
+     * @return (String) correo
+     */
     public String getCorreo() {
         return correo;
     }
--- src/ec/edu/ups/main/Principal.java
+++ src/ec/edu/ups/main/Principal.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package ec.edu.ups.main;
+import ec.edu.ups.clases.*;
 
 /**
  *
  * @author Josze
  */
 public class Principal {
-    
+
+    public static void main(String[] args) {
+        Estudiante est = new Estudiante();
+        est.setNombre("Josze");
+        est.setCodigo(158);
+        est.setCedula("0706740610");
+        est.setDireccion("no me acuerdo de las calles");
+        est.setTelefono("0982432751");
+        est.setCorreo("jo.an.ra.k_@hotmail.com");
+        
+        Sede sed = new Sede();
+        sed.setNombre("Cuenca");
+        sed.setCodigo(542);
+        sed.setDireccion("Detras del parque");
+        sed.setTelefono("2983418");
+        
+        est.setSede(sed);
+        
+        System.out.println(est);
+    }
 }
